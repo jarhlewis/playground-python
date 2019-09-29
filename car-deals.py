12f0d748af5f497553476a6ddb3f3ae7242a1eab
Jarh Lewis <jarhra96@gmail.com>
2019-09-28 23:32:18 -0400

    App can return name and price for all deals rendered in corotos. Install xlsx writer lib to export this to an excel file

--- car-deals.py
+++ car-deals.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup as soup
 from selenium import webdriver
 
-#Get and run the script for that url
+#Create chrome isntance
 driver = webdriver.Chrome("C:/Users/HP/Anaconda3/pkgs/python-chromedriver-binary-77.0.3865.40.0-py37_0/Lib/site-packages/chromedriver_binary/chromedriver.exe")
-driver.get("https://www.corotos.com.do/c/veh%C3%ADculos")
+
+#request url
+driver.get("https://www.corotos.com.do/l/santo-domingo/sc/veh%C3%ADculos/carros")
+
+#Execute script to retreive dynamically rendered html text into res var
 res = driver.execute_script("return document.documentElement.outerHTML")
 
 driver.quit()
@@ -12,11 +16,18 @@ driver.quit()
 cvehicleSoup = soup(res, "html.parser")
 
 #Grab each product
-fullDiv = cvehicleSoup.findAll("div", {"class" : "DbXTC _2pm69 _1JgR4 QF_XG"})
-len(fullDiv)
+# fullDiv = cvehicleSoup.findAll("div", {"class" : "DbXTC _2pm69 _1JgR4 QF_XG"})
+# print(fullDiv)
 
 #Item price and name sub-div has class="_32PML"
 priceNameDiv = cvehicleSoup.findAll("div", {"class" : "_32PML"})
-len(priceNameDiv)
+
+#iterate through. findChildren() returns an array of all children elements which is then searched [0] for price and [2] for name of ad. 
+numberofitems = 0
+for item in priceNameDiv:
+    print("Ad number: " + str(numberofitems))
+    print(item.findChildren()[0].getText())
+    print(item.findChildren()[2].getText())
+    numberofitems = numberofitems + 1
 
 exit()
