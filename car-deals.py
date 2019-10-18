faba70968f19be8cc514a9d7a25e1ba4d752c0da
Jarh Lewis <jarhra96@gmail.com>
2019-10-17 20:44:31 -0400

    Arranged headers as per client request. Add name of seller

--- car-deals.py
+++ car-deals.py
@@ -3,11 +3,8 @@ from selenium import webdriver
 import xlsxwriter
 import sys, os, inspect
 
-if getattr(sys, 'frozen', False) :
-    # running in a bundle
-    chromedriver_path = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile(inspect.currentframe() ))[0]))
-else:
-    chromedriver_path = "C:/Users/HP/Anaconda3/pkgs/python-chromedriver-binary-77.0.3865.40.0-py37_0/Lib/site-packages/chromedriver_binary/chromedriver.exe"
+
+chromedriver_path = "C:/Users/HP/Anaconda3/pkgs/python-chromedriver-binary-77.0.3865.40.0-py37_0/Lib/site-packages/chromedriver_binary/chromedriver.exe"
 
 #dynamically get desktop
 excel_file_path = os.path.join(os.environ["HOMEPATH"], "Desktop")
@@ -47,18 +44,16 @@ worksheet = workbook.add_worksheet()
 row=1 #rows start at 0 but titles go on first row
 
 worksheet.write(0,0, "Fecha")
-worksheet.write(0,1, "Ubicación")
-worksheet.write(0,2, "Categoria")
-worksheet.write(0,3, "Precio")
-worksheet.write(0,4, "Marca")
-worksheet.write(0,5, "Modelo")
-worksheet.write(0,6, "Tipo")
-worksheet.write(0,7, "Año")
-worksheet.write(0,8, "Kilometraje")
-worksheet.write(0,9, "Combustible")
-worksheet.write(0,10, "Transmisión")
-worksheet.write(0,11, "Condición")
-worksheet.write(0,12, "Enlace")
+worksheet.write(0,1, "Precio")
+worksheet.write(0,2, "Marca")
+worksheet.write(0,3, "Modelo")
+worksheet.write(0,4, "Año")
+worksheet.write(0,5, "Tipo")
+worksheet.write(0,6, "Combustible")
+worksheet.write(0,7, "Transmisión")
+worksheet.write(0,8, "Nombre del Vendedor")
+worksheet.write(0,9, "Lugar")
+worksheet.write(0,10, "Enlace")
 
 #driver.get each link item, parse the html for the ad page, create list with infos in
 for link in offer_list:
@@ -67,9 +62,11 @@ for link in offer_list:
     cvehicleSoup = soup(res, "html.parser")
     infos = []
     infos = cvehicleSoup.findAll("p", {"class" : "_15IPb"})
+    location = infos[1].text
     price = cvehicleSoup.find("h2", {"class" : "_2Xz9N l-BkY sQocJ"})
     price = price.findChildren()
     price = price[0]['data-value']
+    nameOfSeller = cvehicleSoup.find("h3", {"class" : "_2Xz9N _1q9YR h3"})
     cleanInfos = []
     for n in infos:
         cleanInfos.append(n.text)
@@ -79,18 +76,16 @@ for link in offer_list:
         fecha = fecha.split(' ')
         fecha = fecha[1] + '/' + fecha[3] + '/' + fecha[5] 
         worksheet.write(row, 0, fecha)
-        worksheet.write(row, 1, cleanInfos[1])
-        worksheet.write(row, 2, cleanInfos[2])
-        worksheet.write(row, 3, price)
-        worksheet.write(row, 4, cleanInfos[cleanInfos.index('Marca') + 1])
-        worksheet.write(row, 5, cleanInfos[cleanInfos.index('Modelo') + 1])
-        worksheet.write(row, 6, cleanInfos[cleanInfos.index('Tipo') + 1])
-        worksheet.write(row, 7, cleanInfos[cleanInfos.index('Año') + 1])
-        worksheet.write(row, 8, cleanInfos[cleanInfos.index('Kilometraje') + 1])
-        worksheet.write(row, 9, cleanInfos[cleanInfos.index('Combustible') + 1])
-        worksheet.write(row, 10, cleanInfos[cleanInfos.index('Transmisión') + 1])
-        worksheet.write(row, 11, cleanInfos[cleanInfos.index('Condición') + 1])
-        worksheet.write(row, 12, link)
+        worksheet.write(row, 1, price)
+        worksheet.write(row, 2, cleanInfos[cleanInfos.index('Marca') + 1])
+        worksheet.write(row, 3, cleanInfos[cleanInfos.index('Modelo') + 1])
+        worksheet.write(row, 4, cleanInfos[cleanInfos.index('Año') + 1])
+        worksheet.write(row, 5, cleanInfos[cleanInfos.index('Tipo') + 1])
+        worksheet.write(row, 6, cleanInfos[cleanInfos.index('Combustible') + 1])
+        worksheet.write(row, 7, cleanInfos[cleanInfos.index('Transmisión') + 1])
+        worksheet.write(row, 8, nameOfSeller.text)
+        worksheet.write(row, 9, location)
+        worksheet.write(row, 10, link)
         row+=1
     except:
         pass
