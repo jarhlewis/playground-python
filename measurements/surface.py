0f624689e6d16b72eed3aebefd1ae397e5ac6c6e
Jarh Lewis <jarhra96@gmail.com>
2023-10-15 19:09:23 +0200

    Refactor to use command line args

--- measurements/surface.py
+++ measurements/surface.py
@@ -1,3 +1,9 @@
+import argparse, sys
+
+ARG_LENGTH = "L"
+ARG_HEIGHT = "H"
+ARG_WIDTH = "W"
+
 def surfaceArea(length = 0, height = 0, width = 0):
     frontSurface = length * height
     topSurface = length * width
@@ -9,4 +15,20 @@ def surfaceArea(length = 0, height = 0, width = 0):
     print(f'Total surface: {totalSurface}')
     print(f'Minimum total surface: {minTotalSurface}')
 
-surfaceArea(1.92, 0.60, 0.63)
+def parseArguments():
+    parser=argparse.ArgumentParser()
+    parser.add_argument(f'-{ARG_LENGTH}', help="Length")
+    parser.add_argument(f'-{ARG_HEIGHT}', help="Height")
+    parser.add_argument(f'-{ARG_WIDTH}', help="Width")
+    return vars(parser.parse_args())
+
+args = parseArguments()
+length = float(args.get(ARG_LENGTH))
+height = float(args.get(ARG_HEIGHT))
+width = float(args.get(ARG_WIDTH))
+
+print(f'Using length: {length}')
+print(f'Using height: {height}')
+print(f'Using width: {width}')
+
+surfaceArea(length, height, width)
